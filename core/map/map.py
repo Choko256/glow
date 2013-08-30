262a420c972681d41684a35698b4c4c9637daa00
Mike Choko <choko@chaksoft.fr>
2013-08-30 21:04:02 +0200

    Map

---
+++ core/map/map.py
@@ -0,0 +1,7 @@
+#-*- coding:utf8 -*-
+
+# Glow Map class
+
+class Map:
+	def __init__(self, name):
+		self._name = name
--- core/player.py
+++ core/player.py
@@ -13,3 +13,17 @@ class Player:
 		self._experience = 0
 		self._money = 0
 		self._title = None
+		self._inventory = []
+
+	def buy_object(self, _object):
+		self._money -= _object._value
+		self.add_object(_object)
+
+	def add_object(self, _object):
+		self._inventory.append(_object)
+
+	def drop_object(self, _object):
+		self._inventory.remove(_object)
+
+	def set_title(self, _title):
+		self._title = _title
