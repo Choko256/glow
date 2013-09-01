bd8840671b3c994402f07ecf39d26ea64bcf1507
Mike Choko <choko@chaksoft.fr>
2013-09-01 02:06:47 +0200

    Resource files  and editor skeleton

--- core/map/map.py
+++ core/map/map.py
@@ -2,6 +2,12 @@
 
 # Glow Map class
 
+class MapTile:
+	def __init__(self, x, y, _type):
+		self._x = x
+		self._y = y
+		self._type = _type
+
 class Map:
 	def __init__(self, name):
 		self._name = name
